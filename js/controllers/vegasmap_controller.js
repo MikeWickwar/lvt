9754179b55d4d25bf608f61ae81e85681517739f
Mike Wickwar <mike.wickwar@gmail.com>
2016-08-04 23:17:38 -0600

    nice looking start on dash, vegas heat deal map in place and working/wired up for downtown. next i should build one for the strip. nice looking background add for dash

--- js/controllers/vegasmap_controller.js
+++ js/controllers/vegasmap_controller.js
@@ -30,9 +30,9 @@ var dealLocations = [
         long : -115.143077
     },
     {
-        casino : 'freemont experience',
+        casino : 'freemont hotel and casino',
         desc : 'This casino is aiiiiite!',
-        lat : 36.170488,
+        lat : 36.170539,
         long : -115.142809
     },
     {
@@ -40,10 +40,53 @@ var dealLocations = [
         desc : 'This is the second best casino in the world!',
         lat : 36.1697672,
         long : -115.1437154
+    },
+    {
+        casino : 'Golden Nugget',
+        desc : 'If you\'re looking for you\'re golden nugget, you might find it here',
+        lat : 36.1703267,
+        long : -115.1457436
+    },
+    {
+        casino : 'The Plaza',
+        desc : 'Go to the plaza and ask for the Mikey D Special $$$',
+        lat : 36.1713467,
+        long : -115.1470735
+    },
+    {
+        casino : 'Golden Gate',
+        desc : '2 for 1 \'you call it\'s\' all night long!',
+        lat : 36.1711016,
+        long : -115.1462776
+    },
+    {
+        casino : 'California',
+        desc : '$10 match play on all dope ass bets!!',
+        lat : 36.1729785,
+        long : -115.1448324
+    },
+    {
+        casino : 'Downtown Grand',
+        desc : 'This is the downtown ain\'t it grand! $5 poker hands',
+        lat : 36.1720691,
+        long : -115.1411417
+    },
+    {
+        casino : 'Gold Spike',
+        desc : 'Spike your Gold!',
+        lat : 36.1711121,
+        long : -115.140793
+    },
+    {
+        casino : 'El Cortez',
+        desc : 'Go to the back nock 3 times and kick the door. password is "boobs". you\'re welcome',
+        lat : 36.1688819,
+        long : -115.1391032
     }
 ];
 //Angular App Module and Controller
 app.controller('VegasMapCtrl', function ($scope) {
+  $("html").css('background-image', 'url(https://www.excalibur.com/content/dam/MGM/excalibur/casino/poker/excalibur-casino-poker-chips.tif)')
   var lasVegas = new google.maps.LatLng(36.170488, -115.142809);
 
   $scope.map = new google.maps.Map(document.getElementById('vegasMap'), {
