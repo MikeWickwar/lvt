6d258cf496fb46e5aab57b3c9d6973077041d4d7
Mike Wickwar <mike.wickwar@gmail.com>
2016-12-04 14:55:19 -0700

    style format in and working, addition of gauge working

--- js/controllers/dash_controller.js
+++ js/controllers/dash_controller.js
@@ -14,4 +14,15 @@ app.controller('DashCtrl', ['$scope','$http','$q', '$state', 'mapService',
   $scope.guest = true;
 
 
+//testing gauge action here
+setInterval(function() {
+  var newVal = Math.floor((Math.random() * 179) + 1);
+
+  $('.gauge--3 .semi-circle--mask').attr({
+    style: '-webkit-transform: rotate(' + newVal + 'deg);' +
+    '-moz-transform: rotate(' + newVal + 'deg);' +
+    'transform: rotate(' + newVal + 'deg);'
+   });
+}, 1000);
+
   }])
